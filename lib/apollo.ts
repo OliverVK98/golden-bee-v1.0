df342eb4b268e2288abf1805acda11323cd00f45
OliverVK98 <v1155533@gmail.com>
2023-03-01 23:32:12 -0500

    Apollo URL update

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,8 +1,8 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-    uri: `${process.env.VERCEL_URL}/api/graphql`,
+    uri: `https://${process.env.VERCEL_URL}/api/graphql`,
     cache: new InMemoryCache(),
-})
+});
 
 export default apolloClient
