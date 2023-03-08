e22087afb3593ced98d118c1afaa33c8312ec5d2
OliverVK98 <v1155533@gmail.com>
2023-03-08 01:39:55 -0500

    Stripe Update

--- utils/email-api-helpers/email-service.ts
+++ utils/email-api-helpers/email-service.ts
@@ -2,7 +2,7 @@ import axios, {AxiosResponse} from "axios";
 
 export default class EmailService {
     static async submitUserEmail(email: string): Promise<AxiosResponse<any>> {
-        return axios.post(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/customers/subscribe`, {
+        return axios.post(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/customers/subscribe`, {
             email
         })
     }
